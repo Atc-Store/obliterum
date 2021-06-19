2be6f6a2829e6376c23ba22d623fa65e10306051
Raeis-Eikr <gabet@DESKTOP-EEF64LE>
2021-06-19 19:54:42 +0900

    Added Enlightenment enchant Added Obliterum creative tab Moved all Obliterum items to Obliterum creative tab

--- src/main/java/net/mcreator/obliterum/item/ObliterumIngotItem.java
+++ src/main/java/net/mcreator/obliterum/item/ObliterumIngotItem.java
@@ -5,10 +5,10 @@ import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.block.BlockState;
 
+import net.mcreator.obliterum.itemgroup.ObliterumItemGroup;
 import net.mcreator.obliterum.ObliterumModElements;
 
 @ObliterumModElements.ModElement.Tag
@@ -25,7 +25,7 @@ public class ObliterumIngotItem extends ObliterumModElements.ModElement {
 	}
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
-			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			super(new Item.Properties().group(ObliterumItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("obliterum_ingot");
 		}
 
--- src/main/java/net/mcreator/obliterum/item/ObliterumSwordItem.java
+++ src/main/java/net/mcreator/obliterum/item/ObliterumSwordItem.java
@@ -6,10 +6,10 @@ import net.minecraftforge.registries.ObjectHolder;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
 
+import net.mcreator.obliterum.itemgroup.ObliterumItemGroup;
 import net.mcreator.obliterum.ObliterumModElements;
 
 @ObliterumModElements.ModElement.Tag
@@ -46,7 +46,7 @@ public class ObliterumSwordItem extends ObliterumModElements.ModElement {
 			public Ingredient getRepairMaterial() {
 				return Ingredient.fromStacks(new ItemStack(ObliterumIngotItem.block, (int) (1)));
 			}
-		}, 3, -3f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}, 3, -3f, new Item.Properties().group(ObliterumItemGroup.tab)) {
 		}.setRegistryName("obliterum_sword"));
 	}
 }
